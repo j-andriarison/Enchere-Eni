21c5432e996d1ba166d2198356466b411d079ea7
Jean ANDRIANARISON <j.andrianarison@outlook.fr>
2021-01-27 01:14:07 +0100

    Sources initiales

---
+++ Enchere-Eni/src/main/java/fr/cda/eni/encherir/EnchereEniApplication.java
@@ -0,0 +1,13 @@
+package fr.cda.eni.encherir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnchereEniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnchereEniApplication.class, args);
+	}
+
+}
---
+++ Enchere-Eni/src/test/java/fr/cda/eni/encherir/EnchereEniApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.cda.eni.encherir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnchereEniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
